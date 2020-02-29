5ffdfe656e0fcb10417b00a90c1d814cb31aa4e1
shohei-yano <shohei-yano@cybozu.co.jp>
2020-02-28 22:27:11 -0800

    update webpack setting

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+module.exports = {
+  entry: {main: './src/js/index.js'},
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename:'[name]_bundle.js'
+  },
+  resolve: {
+    alias: {
+      modules: path.join(__dirname, 'node_modules'),
+      common: path.join(__dirname, 'common')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            },
+          },
+        ]
+      },
+      {
+        test: /.(png|jpg|jpeg|gif|svg|woff|woff2|eot|ttf)(\?v=\d+\.\d+\.\d+)?$/i,
+        use: [
+          'url-loader'
+        ]
+      },
+    ]
+  },
+};
