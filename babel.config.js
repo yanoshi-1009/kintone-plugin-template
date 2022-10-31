c71200dd7069bf5f2542c7c5e158e52ae250119f
yanoshi-1009 <y.idakko@gmail.com>
2022-10-31 23:13:59 +0900

    fix

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = function (api) {
   api.cache(true);
 
   const presets = [
-    ["@babel/preset-env"],
+    "@babel/preset-env",
     "@babel/preset-react",
     "@babel/preset-typescript"
   ];
