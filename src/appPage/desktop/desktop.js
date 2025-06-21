af8dc949c9805ca146131f7b4d32f9fb9f36c4ed
yanoshi-1009 <y.idakko@gmail.com>
2025-06-21 13:55:43 +0900

    drop sample code

--- src/appPage/desktop/desktop.js
+++ src/appPage/desktop/desktop.js
@@ -2,7 +2,4 @@
   "use strict";
   const CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
   console.log(CONFIG);
-  kintone.events.on("app.record.index.show", (event) => {
-    alert("Hello, Kintone!");
-  });
 })(kintone.$PLUGIN_ID);
