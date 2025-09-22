dfc004f041eec8138f6df1d30539cbf70c716912
yanoshi-1009 <y.idakko@gmail.com>
2025-09-22 22:03:11 +0900

    feat: implement button functionality and internationalization for config page

---
+++ src/common/config/constants.ts
@@ -0,0 +1,8 @@
+export const classNames = {
+  cancelButton: "cancel-button",
+  saveButton: "save-button"
+};
+export const ids = {
+  configRoot: "plugin-config-root",
+  configFooter: "plugin-config-footer"
+};
--- src/configPage/config.ts
+++ src/configPage/config.ts
@@ -1,5 +1,52 @@
-((PLUGIN_ID) => {
+import { Button, Spinner, Notification } from "kintone-ui-component";
+import { classNames, ids } from "../common/config/constants";
+import i18n from "../common/i18n/i18n";
+
+((PLUGIN_ID: string) => {
   "use strict";
   const CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
   console.log(CONFIG);
+
+  const cancelButton = new Button({
+    text: i18n.t("config.label.cancel"),
+    type: "normal",
+    className: classNames.cancelButton
+  });
+
+  cancelButton.addEventListener("click", () => {
+    history.back();
+  });
+
+  const saveButton = new Button({
+    text: i18n.t("config.label.save"),
+    type: "submit",
+    className: classNames.saveButton
+  });
+
+  saveButton.addEventListener("click", async () => {
+    const newConfig = {};
+    kintone.plugin.app.setConfig(newConfig);
+  });
+
+  const settingFooter = document.getElementById(
+    ids.configFooter
+  ) as HTMLDivElement;
+  settingFooter.appendChild(cancelButton);
+  settingFooter.appendChild(saveButton);
+
+  // Main
+  const notification = new Notification({
+    type: "danger"
+  });
+  const spinner = new Spinner();
+  spinner.open();
+  try {
+    // foo()
+  } catch (error) {
+    notification.text = (error as Error).message;
+    notification.open();
+    console.error(error);
+  } finally {
+    spinner.close();
+  }
 })(kintone.$PLUGIN_ID);
