c3c00a8c4815840e44115199d98a3fe6db89e5e5
yanoshi-1009 <y.idakko@gmail.com>
2025-09-23 22:10:34 +0900

    fix(PluginFooterBuilder): update button labels to use correct i18n keys for configuration

--- src/configPage/components/PluginFooterBuilder.ts
+++ src/configPage/components/PluginFooterBuilder.ts
@@ -3,11 +3,11 @@ import i18n from "../../common/i18n/i18n";
 
 export default class {
   #saveButton = new Button({
-    text: i18n.t("setting.label.save"),
+    text: i18n.t("config.label.save"),
     type: "submit"
   });
   #cancelButton = new Button({
-    text: i18n.t("setting.label.cancel"),
+    text: i18n.t("config.label.cancel"),
     type: "normal"
   });
   constructor(onSaveClick: () => void) {
