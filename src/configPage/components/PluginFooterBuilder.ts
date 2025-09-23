ebe4611677c702c1e36a6f04f25dadbf100df112
yanoshi-1009 <y.idakko@gmail.com>
2025-09-23 21:48:28 +0900

    feat: implement PluginFooterBuilder with save and cancel buttons

---
+++ src/configPage/components/PluginFooterBuilder.ts
@@ -0,0 +1,24 @@
+import { Button } from "kintone-ui-component";
+import i18n from "../../common/i18n/i18n";
+
+export default class {
+  #saveButton = new Button({
+    text: i18n.t("setting.label.save"),
+    type: "submit"
+  });
+  #cancelButton = new Button({
+    text: i18n.t("setting.label.cancel"),
+    type: "normal"
+  });
+  constructor(onSaveClick: () => void) {
+    this.#saveButton.addEventListener("click", onSaveClick);
+    this.#cancelButton.addEventListener("click", () => {
+      history.back();
+    });
+  }
+
+  appendTo(element: HTMLElement) {
+    element.appendChild(this.#cancelButton);
+    element.appendChild(this.#saveButton);
+  }
+}
