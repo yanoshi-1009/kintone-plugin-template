db2b45eadbd9bfc961996197bf533bf6daa61f50
yanoshi-1009 <y.idakko@gmail.com>
2026-03-03 11:22:11 +0900

    refactor: update PluginFooterBuilder to handle async save operation and simplify config page footer

--- src/configPage/components/PluginFooterBuilder.ts
+++ src/configPage/components/PluginFooterBuilder.ts
@@ -1,6 +1,5 @@
 import { Button } from "kintone-ui-component";
 import i18n from "../../common/i18n/i18n";
-
 export default class {
   #saveButton = new Button({
     text: i18n.t("config.label.save"),
@@ -10,8 +9,11 @@ export default class {
     text: i18n.t("config.label.cancel"),
     type: "normal"
   });
-  constructor(onSaveClick: () => void) {
-    this.#saveButton.addEventListener("click", onSaveClick);
+
+  constructor(onSave: () => Promise<void>) {
+    this.#saveButton.addEventListener("click", async () => {
+      await onSave();
+    });
     this.#cancelButton.addEventListener("click", () => {
       history.back();
     });
--- src/configPage/config.ts
+++ src/configPage/config.ts
@@ -1,47 +1,28 @@
-import { Button, Spinner, Notification } from "kintone-ui-component";
-import { classNames, ids } from "../common/config/constants";
-import i18n from "../common/i18n/i18n";
+import { Spinner, Notification } from "kintone-ui-component";
+import { ids } from "../common/config/constants";
+import type { PluginConfig } from "../common/types/PluginConfig";
+import PluginFooterBuilder from "./components/PluginFooterBuilder";
 
 ((PLUGIN_ID: string) => {
   "use strict";
-  const CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
-  console.log(CONFIG);
+  const config = kintone.plugin.app.getConfig(PLUGIN_ID);
 
-  const cancelButton = new Button({
-    text: i18n.t("config.label.cancel"),
-    type: "normal",
-    className: classNames.cancelButton
-  });
-
-  cancelButton.addEventListener("click", () => {
-    history.back();
-  });
-
-  const saveButton = new Button({
-    text: i18n.t("config.label.save"),
-    type: "submit",
-    className: classNames.saveButton
-  });
-
-  saveButton.addEventListener("click", async () => {
-    const newConfig = {};
-    kintone.plugin.app.setConfig(newConfig);
-  });
-
-  const settingFooter = document.getElementById(
+  const settingFooterDiv = document.getElementById(
     ids.configFooter
   ) as HTMLDivElement;
-  settingFooter.appendChild(cancelButton);
-  settingFooter.appendChild(saveButton);
+  const footer = new PluginFooterBuilder(async () => {
+    const newConfig: PluginConfig = {};
+    kintone.plugin.app.setConfig(newConfig);
+  });
+  footer.appendTo(settingFooterDiv);
 
-  // Main
   const notification = new Notification({
     type: "danger"
   });
   const spinner = new Spinner();
   spinner.open();
   try {
-    // foo()
+    // Main process of config page
   } catch (error) {
     notification.text = (error as Error).message;
     notification.open();
